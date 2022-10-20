99fa6e68a8b9306217d3cb4ef244d171bebcaa45
dangheo19 <109233950+dangheo19@users.noreply.github.com>
2022-10-20 15:59:49 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  introTitle: "Hello bé Nhi iu dấu của a à!",
+  introDesc: `Trái đất vốn lạ thường\n
+    Mà sao em cứ đi nhầm đường\n
+    Lạc vào tim anh lẻ loi\n
+    Đằng sau chữ yêu đây là thương`,
+  btnIntro: "=))",
+  title: "Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙",
+  desc: "Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ",
+  btnYes: "Vẫn cứ là iu anh <3",
+  btnNo: "Hok >.<",
+  question: "Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3",
+  btnReply: "Gửi cho anh <3",
+  reply: "Bởi vì e lun có a trong tim :))",
+  mess: "Anh biết mà 🥰. Yêu em nhìu nhìu 😘",
+  messDesc: "Đi cùng anh đến tận cùng nhé babee :3",
+  btnAccept: "Oki lun =)",
+  messLink: "https://m.me/dangheo19",
+};
